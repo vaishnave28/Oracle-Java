e02405b141c1834a037eb3cae4f96ed7268c3656
vaishnave28 <152139901+vaishnave28@users.noreply.github.com>
2024-07-13 12:21:35 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double number = 0.6;
+        System.out.println("Square: " + (number * number));
+        System.out.println("Cube: " + (number * number * number));
+    }
+}
