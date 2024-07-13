7546ebb3cb650928af975e9aff5d21049eaf5a63
vaishnave28 <152139901+vaishnave28@users.noreply.github.com>
2024-07-13 12:22:54 +0530

    Create MainnthOdd.java

---
+++ MainnthOdd.java
@@ -0,0 +1,7 @@
+public class MainnthOdd {
+    public static void main(String[] args) {
+        int n = 7;
+        int nthOdd = 2 * (2 * n) - 1;
+        System.out.println("nth odd number after " + n + " odd numbers: " + nthOdd);
+    }
+}
